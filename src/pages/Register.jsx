416f4849e64f01ec1b36d0186ca2f990aaa397b1
Jenny Evertsson <jentap@gmail.com>
2025-03-13 18:05:00 +0100

    Registration now works if you API endpoint is called addUser and accepts a ParkingDTO containing userName and LicencePlate

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,33 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import { backendURL } from '../../config';
+
+
 export const Register = () => {
+
+    const [newUser, setNewUser] = useState(null);
+
+    useEffect(() => {
+        if (newUser !== null) {
+            axios.post(`${backendURL}/addUser`, newUser)
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error("Error fetching data:", error);
+                });
+        }
+    }, [newUser]);
+
+    function doit() {
+        // Kommenterar bort så det inte reggas nya användare hela tiden medan jag testar.
+        // setNewUser({username: "Karl", password: "password", email: 'karl@yahoo.se', licensePlate: 'nnn999'});
+    }
+
     return (
-        <h1>Register</h1>
+        <div id="registerPage">
+            <h1>Register</h1>
+            <button onClick={doit}>REGISTER</button>
+        </div>
     )
 }
