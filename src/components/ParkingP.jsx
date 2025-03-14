f34675c4e697b398ff2c0dc954b6c8e51727cac0
Jenny Evertsson <jentap@gmail.com>
2025-03-14 11:05:44 +0100

    Changed function from doStuff to more useful name

--- src/components/ParkingP.jsx
+++ src/components/ParkingP.jsx
@@ -4,20 +4,16 @@ import { useNavigate } from 'react-router';
 export const ParkingP = ({appUser}) => {
     const navigate = useNavigate();
 
-    function doStuff(){
+    function InitiateStartParking(){
         navigate("start-parking");
     }
     const PrintP=({appUser}) => {
         if (appUser) {
-            console.log(appUser.cars);
             return ('P')
-
             }
-            
-            
     }
     return (
-        <div className={styles.bigP} onClick={doStuff}>
+        <div className={styles.bigP} onClick={InitiateStartParking}>
             <PrintP appUser={appUser} />
         </div>
     )
