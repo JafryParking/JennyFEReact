6efbb42797ab471c86464c842667c875efcc9a35
Jenny Evertsson <jentap@gmail.com>
2025-03-14 11:03:31 +0100

    Now lists all cars belonging to the logged in user. No functionality beyong this yet.

--- src/pages/StartParking.jsx
+++ src/pages/StartParking.jsx
@@ -1,10 +1,30 @@
+import styles from './user.module.css';
 export const StartParking = ({appUser}) => {
+
+     const ListAllCars = () => {
+            if (!appUser.cars || appUser.cars.length < 1) {
+                return (
+                    <div>Add cars - print form here</div>
+                );
+            } else
+            return (
+                <div id="cars">
+                {appUser.cars.map((car, index) => { 
+                    return (
+                        <div key={index} className={styles.car}>{car.licencePlate}</div>
+                    )
+                })}
+               </div>
+            )
+         }
+
     if (appUser) {
     return (
         
         <>
         <h1>Start Parking</h1>
         {appUser.userName}'s cars:
+        <ListAllCars />
         </>
     )}
      else {return <h1>Start Parking-No user</h1>}
