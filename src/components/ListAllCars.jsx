8770fb2fcb886e38bebcf1115f293459415dcee8
Jenny Evertsson <jentap@gmail.com>
2025-03-25 12:33:24 +0100

    Clean up code on ListAllCars - remove unused lines

--- src/components/ListAllCars.jsx
+++ src/components/ListAllCars.jsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from "react";
 import styles from "../pages/user.module.css";
-import liststyles from "./listallCars.module.css";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import { FaParking, FaStopCircle} from "react-icons/fa";
+import {  FaStopCircle} from "react-icons/fa";
 import { MdLocalParking } from "react-icons/md";
-import { backendURL } from '../../config';
+
 import { useTogglePark } from "./ToggleParking";
 
 
@@ -16,39 +13,7 @@ export const ListAllCars = ({ appUser, setAppUser, cars }) => {
         setUserCars(cars);
     }, []); // Runs only on mount
     
-    
     const toggleParkThisCar = useTogglePark();
-    // const toggleParkThisCar = (regPlate) => {
-    //     let isParkedNow = appUser?.isParked?.some(parked => parked.regPlate === regPlate) || false;  
-    //     if (!isParkedNow) {
-    //         // Not parked > Sent to backend to start Parking
-    //         axios({
-    //             method: 'post',
-    //             url: `${backendURL}/startParking`,
-    //             data: {
-    //               userID: appUser.id,
-    //               regPlate: regPlate
-    //             }
-    //           })
-    //           .then(response => {
-    //             setAppUser(response.data);
-    //           })
-    //           .catch(error => {
-    //             console.error("Error fetching data:", error);
-    //         });
-    //     } else {
-    //         // Car is parked -> Sent to backend to stop parking
-    //         axios.get(`${backendURL}/stopParking/${regPlate}`)
-    //             .then(response => {
-    //                 let fee = new Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(response.data.fee);
-    //                 alert(`${fee} kr added to your account.`);
-    //                 setAppUser(response.data.user);
-    //             })
-    //             .catch(error => {
-    //                 console.error("Error fetching data:", error);
-    //             });
-    //     }
-    // }
 
     return (
         <>
